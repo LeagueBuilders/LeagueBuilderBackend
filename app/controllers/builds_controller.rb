5ffd2514fc4fc5109556fda72d93eed70e053cd7
Frank Gerold <FrankETSGerold@gmail.com>
2020-01-23 13:06:29 -0500

    show

--- app/controllers/builds_controller.rb
+++ app/controllers/builds_controller.rb
@@ -2,6 +2,14 @@ class BuildsController < ApplicationController
 
   def create
     build = Build.create(champion: params[:champion], inventory: params[:inventory], user: User.first)
+
+    render json: build.to_json
+  end
+
+  def show
+    let builds = Build.find_all_by(champion: params[:champion])
+
+    render json: builds.to_json
   end
 
 end
